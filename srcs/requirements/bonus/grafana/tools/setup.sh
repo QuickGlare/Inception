dc091465b609bbd911c98de06c540d39798f3cb5
pcoletta <p.coletta@xquickglare.it>
2023-06-19 18:50:47 +0200

    Updated .env and nginx

--- srcs/requirements/bonus/grafana/tools/setup.sh
+++ srcs/requirements/bonus/grafana/tools/setup.sh
@@ -1,5 +1,6 @@
 grafana cli plugins install grafana-piechart-panel
-grafana cli -config /etc/grafana.ini -homepath /usr/share/grafana admin reset-admin-password $GRAFANA_ADMIN_PASSWORD
+
+grafana cli --homepath "/usr/share/grafana" admin reset-admin-password $GRAFANA_ADMIN_PASSWORD
 
 sed "s/MYSQL_WORDPRESS_DATABASE/$MYSQL_WORDPRESS_DATABASE/1" -i -r /var/lib/grafana/provisioning/datasources/wp_database_provider.yml
 sed "s/MYSQL_USERNAME/$MYSQL_USERNAME/1" -i -r /var/lib/grafana/provisioning/datasources/wp_database_provider.yml
@@ -12,5 +13,8 @@ grafana server \
     cfg:paths.plugins=/var/lib/grafana/plugins \
     cfg:paths.provisioning=/var/lib/grafana/provisioning \
     cfg:server.http_addr=0.0.0.0 \
-    cfg:server.domain=pcoletta.42.fr \
+    cfg:server.domain=$DOMAIN_NAME \
+    cfg:auth.anonymous.enabled=true \
+    cfg:security.disable_initial_admin_creation=true \
     cfg:server.root_url=%\(protocol\)s://%\(domain\)s:%\(http_port\)s/grafana/
+
